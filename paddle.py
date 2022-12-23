f00c0065cc362f5f6cedd4626f72d4387ecad298
Michael Kiruti <mikekirutic@gmail.com>
2022-12-23 09:12:06 +0300

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -14,7 +14,6 @@ LEFT_BORDER = -1 * RIGHT_BORDER
 
 class Paddle(Turtle):
     def __init__(self):
-        ''' initializing Paddle object '''
         super().__init__()
         self.penup()
         self.speed("fastest")
@@ -22,33 +21,34 @@ class Paddle(Turtle):
         self.color('indigo')
         self.reset()
 
+# reset paddle shape and position
     def reset(self):
-        ''' reset paddle shape and position '''
         self.goto(POSITION)
         self.shapesize(stretch_wid=W, stretch_len=L[0])
         self.paddle_l = L[0] * 20
         self.is_pause = False
 
+# set paddle state 
     def pause(self, is_paused):
-        ''' set paddle state '''
         self.is_pause = is_paused
 
+#  move paddle left with key press
     def go_left(self):
-        ''' move paddle left with key press '''
         if not self.is_pause:
             edge = self.xcor() - self.paddle_l / 2
             if edge > LEFT_BORDER:
                 self.bk(STEP)
 
+#  move paddle right with key press
     def go_right(self):
-        ''' move paddle right with key press '''
         if not self.is_pause:
             edge = self.xcor() + self.paddle_l / 2
             if edge < RIGHT_BORDER:
                 self.fd(STEP)
 
+                
+# move paddle with mouse click & drag. y cor is constant
     def mouse_drag(self, x, y):
-        ''' move paddle with mouse click & drag. y cor is constant'''
         y = PADDLE_Y
         if not self.is_pause:
             # self.ondrag(None)
@@ -62,16 +62,16 @@ class Paddle(Turtle):
                 self.goto(x, y)
             # self.ondrag(self.mouse_drag)
 
+# maximal distance hit on the paddle middle
     def max_dis_mid(self):
-        ''' maximal distance hit on the paddle middle '''
         return int(((self.paddle_l // 2 - 20) ** 2 + (PADDLE_W // 2 + 10) ** 2) ** 0.5)
 
+#  maximal distance hit on the paddle corner 
     def max_dis_cor(self):
-        ''' maximal distance hit on the paddle corner '''
         return int(((self.paddle_l // 2 + 10) ** 2 + (PADDLE_W // 2 + 10) ** 2) ** 0.5)
 
+# resize paddle for level 2 
     def resize(self):
-        ''' resize paddle for level 2 '''
         self.shapesize(stretch_wid=W, stretch_len=L[1])
         self.paddle_l = L[1] * 20
         self.goto(POSITION)
